191690cae66a29956084b1c0e7258a07e1431672
Jamilo-school <geofreyonyango167@gmail.com>
2023-04-10 17:29:12 +0300

    oo

--- app.js
+++ app.js
@@ -49,3 +49,35 @@ function events() {
 }
 
 events();
+//////////////////////////////////////////////////////////////////////////////////
+
+  // popupNotification message on log in page created by javascript amd cs;javascript code  starts here//
+
+        // Create the pop up notification
+        var popup = document.createElement("div");
+        popup.classList.add("popup");
+        
+        // Add the notification message to the pop up
+        var message = document.createTextNode("Having a problem updating or viewing the register on my smartphone?🧑‍⚕️We got you covered.Dowload Google Sheet app from playstore or Apple store log in and  open the register once again.Problem solve");
+        popup.appendChild(message);
+        
+        // Add the pop up to the page
+        document.body.appendChild(popup);
+        
+        // Use CSS transitions to create a smooth fade in effect
+        popup.style.transition = "opacity 1.5s ease-in-out";
+        popup.style.opacity = 0;
+        
+        
+        // Show the pop up after a short delay
+        setTimeout(function() {
+          popup.style.opacity = 1;
+        }, 500);
+        
+        // Hide the pop up after a few seconds
+        setTimeout(function() {
+          popup.style.opacity = 0;
+        }, 16000);
+      
+        // End of popup Notification message creted by javascript amd cs;javascript code  ends here//
+        
